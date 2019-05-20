83f44bd27157fa9d7d62153a1d904e14b300c591
Kim Juhui <kjh107704@gmail.com>
2019-05-21 01:10:06 +0900

    Titanic Source Code

---
+++ Titanic/1715016.py
@@ -0,0 +1,45 @@
+import seaborn as sns
+import sys
+import pandas as pd
+import numpy as numpy
+import matplotlib.pyplot as plt
+plt.show()
+sns.set()
+
+train = pd.read_csv(sys.argv[1])
+test = pd.read_csv(sys.argv[2])
+
+'''
+def pie_chart(feature):
+    feature_ratio = train[feature].value_counts(sort=False)
+    feature_size = feature_ratio.size
+    feature_index = feature_ratio.index
+    survived = train[train['Survived'] == 1][feature].value_counts()
+    dead = train[train['Survived'] == 0][feature].value_counts()
+
+    plt.plot(aspect='auto')
+    plt.pie(feature_ratio, labels=feature_index, autopct='%1.1f%%')
+    plt.title(feature + '\'s ratio in total')
+    plt.show()
+
+    for i, index in enumerate(feature_index):
+        plt.subplot(1, feature_size + 1, i + 1, aspect='equal')
+        plt.pie([survived[index], dead[index]], labels=[
+                'Survived', 'Dead'], autopct='%1.1f%%')
+        plt.title(str(index) + '\'s ratio')
+
+    plt.show()
+'''
+# pie_chart('Embarked')
+
+
+def find_NaN(feature):
+    feature_ratio = train[feature].value_counts(sort=False)
+    feature_size = feature_ratio.size
+    feature_index = feature_ratio.index
+
+    print("feature_index = "+feature_index)
+
+
+col = train.columns
+print(col[1])
